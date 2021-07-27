1fa01d0202e359fdc288e0dae75c9811d3aadf6c
Wilmer <wmarchan@factoriamedia.com>
2021-07-27 15:22:44 -0500

    changes

--- lib/src/day_item.dart
+++ lib/src/day_item.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 /// Creates a Widget representing the day.
@@ -27,59 +28,39 @@ class DayItem extends StatelessWidget {
     this.dayNameColor,
   }) : super(key: key);
 
-  final double height = 70.0;
-  final double width = 60.0;
+  final double height = 65.0;
+  final double width = 45.0;
 
   ///? I united both widgets to increase the touch target of non selected days by using a transparent box decorator.
   ///? Now if the user click close to the number but not straight on top it will still select the date. (ONLY INFORMATION - ERASE)
   _buildDay(BuildContext context) {
-    final textStyle = TextStyle(
-      color: available
-        ? dayColor ?? Theme.of(context).accentColor
-        : dayColor?.withOpacity(0.5) ??
-        Theme.of(context).accentColor.withOpacity(0.5),
-      fontSize: 32,
-      fontWeight: FontWeight.normal);
-    final selectedStyle = TextStyle(
-      color: activeDayColor ?? Colors.white,
-      fontSize: 32,
-      fontWeight: FontWeight.bold,
-      height: 0.8,
-    );
-
     return GestureDetector(
       onTap: available ? onTap as void Function()? : null,
       child: Container(
         decoration: isSelected
-          ? BoxDecoration(
-          color:
-          activeDayBackgroundColor ?? Theme.of(context).accentColor,
-          borderRadius: BorderRadius.circular(12.0),
-        )
-          : BoxDecoration(color: Colors.transparent),
+            ? BoxDecoration(
+                color:
+                    activeDayBackgroundColor ?? Theme.of(context).accentColor,
+                borderRadius: BorderRadius.circular(10.0),
+              )
+            : BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10.0),
+              ),
         height: height,
         width: width,
+        margin: EdgeInsets.symmetric(horizontal: 5),
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
-            if (isSelected) ...[
-              SizedBox(height: 7),
-              _buildDots(),
-              SizedBox(height: 12),
-            ] else
-              SizedBox(height: 14),
-            Text(
-              dayNumber.toString(),
-              style: isSelected ? selectedStyle : textStyle,
-            ),
-            if (isSelected)
-              Text(
-                shortName,
+            Text(shortName,
                 style: TextStyle(
-                  color: dayNameColor ?? activeDayColor ?? Colors.white,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 14,
-                ),
-              ),
+                    fontSize: 12,
+                    color: isSelected ? Colors.white : Colors.black)),
+            Text(dayNumber.toString(),
+                style: TextStyle(
+                    fontSize: 12,
+                    color: isSelected ? Colors.white : Colors.black)),
           ],
         ),
       ),
